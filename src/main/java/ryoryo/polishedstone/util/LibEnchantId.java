088fb0a58ccb7cf35eded640e5e8b43fdecad866
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2020-04-28 22:43:54 +0900

    change the way to get enchantment id

--- src/main/java/ryoryo/polishedstone/util/LibEnchantId.java
+++ src/main/java/ryoryo/polishedstone/util/LibEnchantId.java
@@ -1,32 +1,38 @@
 package ryoryo.polishedstone.util;
 
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.init.Enchantments;
+
 public class LibEnchantId
 {
-	public static final int PROTECTION = 0;//ダメージ軽減
-	public static final int FIRE_PROTECTION = 1;//火炎耐性
-	public static final int FEATHER_FALLING = 2;//落下耐性
-	public static final int BLAST_PROTECTION = 3;//爆発耐性
-	public static final int PROJECTILE_PROTECTION = 4;//飛び道具耐性
-	public static final int RESPPIRATION = 5;//水中呼吸
-	public static final int AQUA_AFFINITY = 6;//水中呼吸
-	public static final int THORNS = 7;//棘の鎧
-	public static final int DEPTH_STRIDER = 8;//水中歩行
-	public static final int FROST_WALKER = 9;//氷渡り
-	public static final int SHARPNESS = 16;//ダメージ増加
-	public static final int SMITE = 17;//アンデッド特攻
-	public static final int BANE_OF_ARTHROPODS = 18;//虫特攻
-	public static final int KNOCKBACK = 19;//ノックバック
-	public static final int FIRE_ASPECT = 20;//火属性
-	public static final int LOOTING = 21;//ドロップ増加
-	public static final int EFFICIENCY = 32;//効率強化
-	public static final int SILK_TOUCH = 33;//シルクタッチ
-	public static final int UNBREAKING = 34;//耐久力
-	public static final int FORTUNE = 35;//幸運
-	public static final int POWER = 48;//射撃ダメージ増加
-	public static final int PUNCH = 49;//パンチ
-	public static final int FLAME = 50;//フレイム
-	public static final int INFINITY = 51;//無限
-	public static final int LUCK_OF_THE_SEA = 61;//宝釣り
-	public static final int LURE = 62;//入れ食い
-	public static final int MENDING = 70;//修繕
+	public static final int PROTECTION = Enchantment.getEnchantmentID(Enchantments.PROTECTION);//ダメージ軽減
+	public static final int FIRE_PROTECTION = Enchantment.getEnchantmentID(Enchantments.FIRE_PROTECTION);//火炎耐性
+	public static final int FEATHER_FALLING = Enchantment.getEnchantmentID(Enchantments.FEATHER_FALLING);//落下耐性
+	public static final int BLAST_PROTECTION = Enchantment.getEnchantmentID(Enchantments.BLAST_PROTECTION);//爆発耐性
+	public static final int PROJECTILE_PROTECTION = Enchantment.getEnchantmentID(Enchantments.PROJECTILE_PROTECTION);//飛び道具耐性
+	public static final int RESPIRATION = Enchantment.getEnchantmentID(Enchantments.RESPIRATION);//水中呼吸
+	public static final int AQUA_AFFINITY = Enchantment.getEnchantmentID(Enchantments.AQUA_AFFINITY);//水中採掘
+	public static final int THORNS = Enchantment.getEnchantmentID(Enchantments.THORNS);//棘の鎧
+	public static final int DEPTH_STRIDER = Enchantment.getEnchantmentID(Enchantments.DEPTH_STRIDER);//水中歩行
+	public static final int FROST_WALKER = Enchantment.getEnchantmentID(Enchantments.FROST_WALKER);//氷渡り
+	public static final int BINDING_CURSE = Enchantment.getEnchantmentID(Enchantments.BINDING_CURSE);//束縛の呪い
+	public static final int SHARPNESS = Enchantment.getEnchantmentID(Enchantments.SHARPNESS);//ダメージ増加
+	public static final int SMITE = Enchantment.getEnchantmentID(Enchantments.SMITE);//アンデッド特攻
+	public static final int BANE_OF_ARTHROPODS = Enchantment.getEnchantmentID(Enchantments.BANE_OF_ARTHROPODS);//虫特攻
+	public static final int KNOCKBACK = Enchantment.getEnchantmentID(Enchantments.KNOCKBACK);//ノックバック
+	public static final int FIRE_ASPECT = Enchantment.getEnchantmentID(Enchantments.FIRE_ASPECT);//火属性
+	public static final int LOOTING = Enchantment.getEnchantmentID(Enchantments.LOOTING);//ドロップ増加
+	public static final int SWEEPING = Enchantment.getEnchantmentID(Enchantments.SWEEPING);//範囲ダメージ増加
+	public static final int EFFICIENCY = Enchantment.getEnchantmentID(Enchantments.EFFICIENCY);//効率強化
+	public static final int SILK_TOUCH = Enchantment.getEnchantmentID(Enchantments.SILK_TOUCH);//シルクタッチ
+	public static final int UNBREAKING = Enchantment.getEnchantmentID(Enchantments.UNBREAKING);//耐久力
+	public static final int FORTUNE = Enchantment.getEnchantmentID(Enchantments.FORTUNE);//幸運
+	public static final int POWER = Enchantment.getEnchantmentID(Enchantments.POWER);//射撃ダメージ増加
+	public static final int PUNCH = Enchantment.getEnchantmentID(Enchantments.PUNCH);//パンチ
+	public static final int FLAME = Enchantment.getEnchantmentID(Enchantments.FLAME);//フレイム
+	public static final int INFINITY = Enchantment.getEnchantmentID(Enchantments.INFINITY);//無限
+	public static final int LUCK_OF_THE_SEA = Enchantment.getEnchantmentID(Enchantments.LUCK_OF_THE_SEA);//宝釣り
+	public static final int LURE = Enchantment.getEnchantmentID(Enchantments.LURE);//入れ食い
+	public static final int MENDING = Enchantment.getEnchantmentID(Enchantments.MENDING);//修繕
+	public static final int VANISHING_CURSE = Enchantment.getEnchantmentID(Enchantments.VANISHING_CURSE);//消滅の呪い
 }
