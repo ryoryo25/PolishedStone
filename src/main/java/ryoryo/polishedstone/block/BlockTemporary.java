801c57becb748dceaf15773337907b142ca1b385
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2020-05-02 00:04:49 +0900

    fix item drop when break block

--- src/main/java/ryoryo/polishedstone/block/BlockTemporary.java
+++ src/main/java/ryoryo/polishedstone/block/BlockTemporary.java
@@ -1,9 +1,13 @@
 package ryoryo.polishedstone.block;
 
+import java.util.Random;
+
+import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
@@ -26,38 +30,53 @@ public class BlockTemporary extends BlockModBase
 	@Override
 	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
 	{
-		ItemStack heldItem = player.getHeldItem(hand);
-		if(heldItem != ItemStack.EMPTY)
-		{
-			Item held = heldItem.getItem();
+		ItemStack held = player.getHeldItem(hand);
 
-			if(held instanceof ItemBlock)
+		if(!world.isRemote && !held.isEmpty())
+		{
+			Item heldItem = held.getItem();
+
+			if(heldItem != null && heldItem instanceof ItemBlock)
 			{
-				ItemBlock heldBlock = (ItemBlock) held;
-				if(heldBlock.getBlock() != this)
+				ItemBlock heldBlock = (ItemBlock) heldItem;
+				Block block = heldBlock.getBlock();
+
+				if(block != null && block != this)
 				{
-					int meta = heldBlock.getMetadata(heldItem.getMetadata());
-					IBlockState place = heldBlock.getBlock().getStateForPlacement(world, pos, player.getHorizontalFacing(), hitX, hitY, hitZ, meta, player, player.getActiveHand());
+					int meta = heldBlock.getMetadata(held);
+					IBlockState toPlace = block.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, meta, player, hand);
 
 					world.destroyBlock(pos, false);
-					if(place.getBlock().canPlaceBlockAt(world, pos))
-						world.setBlockState(pos, place);
-					if(!Utils.isCreative(player))
+
+					if(block.canPlaceBlockAt(world, pos))
 					{
-						Utils.giveItemToPlayer(player, new ItemStack(Register.BLOCK_TEMPORARY));
-						heldItem.shrink(1);
+						world.setBlockState(pos, toPlace);
+
+						if(!Utils.isCreative(player))
+						{
+							Utils.giveItemToPlayer(player, new ItemStack(Register.BLOCK_TEMPORARY));
+							held.shrink(1);
+						}
+
+						return true;
 					}
-
-					return true;
+					else
+					{
+						world.setBlockState(pos, Register.BLOCK_TEMPORARY.getDefaultState());
+						Utils.sendChat(player, "This block can't be placed here!");
+						return true;
+					}
 				}
-
-				return false;
 			}
-
-			return false;
 		}
 
 		return false;
+	}
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune)
+	{
+		return Items.AIR;
 	}
 
 	@Override
