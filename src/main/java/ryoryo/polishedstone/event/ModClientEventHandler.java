36c54b71bf14c2c9146404b7455ae24427fc3fd0
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2020-05-15 00:44:15 +0900

    add function to pick block with middle click from far away

--- src/main/java/ryoryo/polishedstone/event/ModClientEventHandler.java
+++ src/main/java/ryoryo/polishedstone/event/ModClientEventHandler.java
@@ -4,17 +4,16 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBiped.ArmPose;
 import net.minecraft.client.model.ModelPlayer;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
-import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import ryoryo.polishedlib.util.Utils;
@@ -86,12 +85,18 @@ public class ModClientEventHandler {
 		}
 	}
 
+	//long creative block pick
 	@SubscribeEvent
-	public void onPickBlockMiddleClick(InputEvent.KeyInputEvent event) {
+	public void onPickBlockMiddleClick(InputEvent.MouseInputEvent event) {
 		if(Minecraft.getMinecraft().gameSettings.keyBindPickBlock.isPressed()) {
-			EntityPlayer player = Minecraft.getMinecraft().player;
-			Vec3d look = player.getLookVec();
+			Minecraft mc = Minecraft.getMinecraft();
+			EntityPlayer player = mc.player;
+			World world = mc.world;
+			//			RayTraceResult ray = mc.objectMouseOver;
+			RayTraceResult ray = player.rayTrace(64.0, mc.getTickLength());
 
+			if(player != null && player.capabilities.isCreativeMode && ray != null && ray.typeOfHit == RayTraceResult.Type.BLOCK)
+				ForgeHooks.onPickBlock(ray, player, world);
 		}
 	}
 
@@ -107,16 +112,6 @@ public class ModClientEventHandler {
 					event.getModelRegistry().putObject(resource, new BakedModelFullbright(event.getModelRegistry().getObject(resource), path));
 				}
 			}
-		}
-
-		@SubscribeEvent
-		public void loadTexture(TextureStitchEvent.Pre event) {
-			TextureMap map = event.getMap();
-
-			if(!map.getBasePath().equals("textures"))
-				return;
-
-			for(int i = 0; i < 8; i++) {}
 		}
 	}
 }
