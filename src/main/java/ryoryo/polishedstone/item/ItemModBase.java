a92ef1823b9ec314ae62077ab9b5f7127a1f0419
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2020-05-18 00:01:43 +0900

    clean up

--- src/main/java/ryoryo/polishedstone/item/ItemModBase.java
+++ src/main/java/ryoryo/polishedstone/item/ItemModBase.java
@@ -6,6 +6,5 @@ import ryoryo.polishedstone.PSV2Core;
 public class ItemModBase extends ItemBase {
 	public ItemModBase(String name) {
 		super(name, PSV2Core.TAB_MOD);
-		this.setUnlocalizedName(name);
 	}
 }
